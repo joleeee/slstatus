361127e802b79242b5ec2442c1bf29f7de2a6e34
Jonas Elvedal Hole <jolejonaseh@gmail.com>
2020-11-08 18:26:27 +0100

    Improve formatting

--- util.c
+++ util.c
@@ -99,10 +99,10 @@ fmt_human(uintmax_t num, int base)
 	double scaled;
 	size_t i, prefixlen;
 	const char **prefix;
-	const char *prefix_1000[] = { "", "k", "M", "G", "T", "P", "E", "Z",
+	const char *prefix_1000[] = { " ", "k", "M", "G", "T", "P", "E", "Z",
 	                              "Y" };
-	const char *prefix_1024[] = { "", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei",
-	                              "Zi", "Yi" };
+	const char *prefix_1024[] = { "B", "k", "M", "G", "T", "P", "E",
+	                              "Z", "Y" };
 
 	switch (base) {
 	case 1000:
@@ -123,7 +123,7 @@ fmt_human(uintmax_t num, int base)
 		scaled /= base;
 	}
 
-	return bprintf("%.1f %s", scaled, prefix[i]);
+	return bprintf("%6.1f%s", scaled, prefix[i]);
 }
 
 int
